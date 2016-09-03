c3b8ccf1da39cbadaf350d1ee978ab58b6775330
TechLady <sushamajarange@gmail.com>
2016-09-03 20:00:07 +0100

    added password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:firstName, :lastName, :email, :password_digest, :mobile, :picture)
+      params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation, :mobile, :picture)
     end
 end
