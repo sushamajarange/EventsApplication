c76af991d70826e53d77aeaabeadb4e5246b4f31
sushama <sushamajarange@gmail.com>
2016-09-06 06:50:52 +0100

    Added Events application with sorting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+	has_many :events
 
+  before_save { self.email = email.downcase }
+  validates :firstName, :lastName,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
